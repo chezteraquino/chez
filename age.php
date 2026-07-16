a9d864575c6db0b9b7813f5d6127f14611155b45
chezteraquino <155541875+chezteraquino@users.noreply.github.com>
2026-07-16 15:50:37 +0800

    Add files via upload

---
+++ age.php
@@ -0,0 +1,19 @@
+<?php
+$age  = 60;
+
+if ($age <= 12) {
+    echo"Child";
+} 
+elseif($age <= 17){
+    echo"Teenager";
+}
+elseif($age <= 59){
+    echo"Adult";
+}
+else {
+    echo"Senior Citizen";
+}
+
+
+
+?>
